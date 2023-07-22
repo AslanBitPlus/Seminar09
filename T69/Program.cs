ec0b39ab728b018ac196433878e0e6319a251ad5
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-22 18:39:37 +0300

    Add T69

---
+++ T69/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число A в целую степень B с помощью рекурсии.
+
+// ==========================
+int PowDigits(int numA, int numB)
+{
+    if (numB == 1) return numA;
+    else return numA * PowDigits(numA, numB - 1);   
+}
+
+// ==========================
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int sumDig = PowDigits(A, B);
+Console.WriteLine();
+Console.Write($"{A} в степени {B} равен {sumDig}");
