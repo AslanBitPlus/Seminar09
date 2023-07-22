f84f11231e04bca0334706d65fa2adefd6aa35e3
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-22 18:02:10 +0300

    Add info T65

--- T67/Program.cs
+++ T67/Program.cs
@@ -3,18 +3,17 @@
 //
 // M = 1; N = 5 => "1, 2, 3, 4, 5"
 // M = 4; N = 8 => "4, 5, 6, 7, 7"
-
+// ==========================
 int SumDigits(int num)
 {
     if (num < 10) return num;
     else return num % 10 + SumDigits(num / 10);   
 }
 
-
+// ==========================
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int sumDig = SumDigits(number);
 Console.WriteLine();
 Console.Write($"Сумма цифр числа {number} равна {sumDig}");
-SumDigits(number);
